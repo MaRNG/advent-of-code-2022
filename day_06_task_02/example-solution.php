68aa874c615760d6bba9345fd86a7b81d27bab76
MaRNG <kubicka.marek@gmail.com>
2022-12-10 14:46:16 +0100

    solve second puzzle of the day 6

---
+++ day_06_task_02/example-solution.php
@@ -0,0 +1,26 @@
+<?php
+
+const STARTING_PACKET_LENGTH = 14;
+
+$bufferStream = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb';
+
+$bufferStreamChars = str_split($bufferStream, 1);
+$bufferStreamCharsCount = count($bufferStreamChars);
+
+if (($bufferStreamCharsCount - STARTING_PACKET_LENGTH) > 0)
+{
+    for ($markerIdx = 0; $markerIdx < ($bufferStreamCharsCount - STARTING_PACKET_LENGTH); $markerIdx++)
+    {
+        $checkStartingPacket = array_slice($bufferStreamChars, $markerIdx, STARTING_PACKET_LENGTH);
+
+        if (array_unique($checkStartingPacket) === $checkStartingPacket)
+        {
+            echo $markerIdx + STARTING_PACKET_LENGTH;
+            exit(0);
+        }
+    }
+}
+else
+{
+    throw new InvalidArgumentException('Incoming packet is too short!');
+}
